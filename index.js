75cb00425e8c16c61f657f0a77f7c0297a174b18
MaGloCon <mgcontrada@gmail.com>
2024-05-14 22:10:36 +0200

    Add back to JWT authentication to user routes

--- index.js
+++ index.js
@@ -37,9 +37,12 @@ const passport = require('passport');
 require('./config/passport.js');
 const auth = require('./config/auth.js')(app); 
 
+app.use('/documentation', express.static('public', {index: 'documentation.html'})); 
+
+
 app.get('/', (req, res) => { res.status(200).send('Welcome to Cinephile!');});
 
-app.use('/documentation', express.static('public', {index: 'documentation.html'})); 
+
 app.get('/movies', movies.readAll);
 app.get('/movies/search', movies.search);
 app.get('/movies/featured', movies.readFeatured);
@@ -49,25 +52,21 @@ app.get('/movies/:title/genre', movies.readGenreByTitle);
 app.get('/movies/director/:name', movies.readDirector);
 app.get('/movies/:title/director', movies.readDirectorByTitle);
 
-app.post('/users/signup', users.create);
-app.get('/users', users.readAll);
-app.get('/user/search/id/:id', users.readById);
-app.get('/user/search/username/:username', users.readByUsername);
-app.get('/users/profile/me', isLoggedIn, users.me); 
-app.post('/users/profile/update', isLoggedIn, users.update); 
-app.post('/users/:Username/movies/:title', isLoggedIn, users.addFavoriteMovie);
-app.delete('/users/:Username/movies/:title', isLoggedIn, users.deleteFavoriteMovie);
-app.delete('/users/profile/delete', isLoggedIn, users.delete); 
-app.post('/logout', function(req, res) { 
-  req.logout();
-  res.status(200).send('Logged out')
-});
+const authenticate = passport.authenticate('jwt', { session: false });
 
-function isLoggedIn(req, res, next) { //make an arrow function and move before usage in above routes?
-    if (req.isAuthenticated())
-        return next();
-    res.end('Not logged in');
-};
+app.post('/users/signup', users.create);
+app.post('/logout', (req, res) => { 
+  req.logout();
+  res.status(200).send('Logged out');
+});
+app.get('/users', users.readAll);
+app.get('/users/search/id/:id', users.readById);
+app.get('/users/search/username/:username', users.readByUsername);
+app.get('/users/profile/me', authenticate, users.me); 
+app.post('/users/profile/update', authenticate, users.update); 
+app.delete('/users/profile/delete', authenticate, users.delete);
+app.post('/users/:Username/movies/:title', authenticate, users.addFavoriteMovie);
+app.delete('/users/:Username/movies/:title', authenticate, users.deleteFavoriteMovie);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
